1843ff82d8d41207c9f211b3b761b007c5d0f898
Darren Elwood <darrenelwood555@gmail.com>
2022-03-09 18:21:49 +0000

    Update oscillators.py
    Fixed "super." instances which should have been "super()."

--- oscillators.py
+++ oscillators.py
@@ -1,6 +1,6 @@
 # Copyright 2022 Darren Elwood <darren@textnode.com> http://www.textnode.com @textnode
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 3.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at 
 #
@@ -73,7 +73,7 @@ class cosine(oscillator):
 
 class square(oscillator):
     def send(self, ignored_arg):
-        super.exhausted()
+        super().exhausted()
         self.modulate()
         val = self.amplitude if self.current_phase % (math.pi * 2) < math.pi else -1.0 * self.amplitude
         self.current_phase = self.current_phase + self.gap
@@ -81,7 +81,7 @@ class square(oscillator):
 
 class sawtooth(oscillator):
     def send(self, ignored_arg):
-        super.exhausted()
+        super().exhausted()
         self.modulate()
         ph = self.current_phase % math.pi
         val = ((2.0 * (ph / math.pi)) - 1.0) * self.amplitude if ph < math.pi else ((2.0 * (1.0 - (ph / math.pi))) - 1.0) * self.amplitude
@@ -90,7 +90,7 @@ class sawtooth(oscillator):
 
 class reverse_sawtooth(oscillator):
     def send(self, ignored_arg):
-        super.exhausted()
+        super().exhausted()
         self.modulate()
         ph = self.current_phase % math.pi
         val = (2.0 * (1 - (ph / math.pi)) - 1.0) * self.amplitude if ph < math.pi else (2.0 * ((ph / math.pi) - 1.0) - 1.0) * self.amplitude
@@ -113,4 +113,3 @@ class constant(Generator):
 
     def throw(self, type=None, value=None, traceback=None):
         raise StopIteration
-
