5ea9e4c9fbcb1a8f5ff0303d9b891a1145752ba2
Darren Elwood <darrenelwood555@gmail.com>
2022-03-09 19:42:42 +0000

    Working copy. Including working midi input.
    Used aconnectgui to connect midi controller output to "Midi Through Port-0" and pygame.midi now receives the events.

--- oscillators.py
+++ oscillators.py
@@ -1,6 +1,6 @@
 # Copyright 2022 Darren Elwood <darren@textnode.com> http://www.textnode.com @textnode
 #
-# Licensed under the Apache License, Version 3.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at 
 #
@@ -113,3 +113,4 @@ class constant(Generator):
 
     def throw(self, type=None, value=None, traceback=None):
         raise StopIteration
+
